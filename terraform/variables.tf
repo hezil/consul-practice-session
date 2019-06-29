1ac6e0f79ee9d56d454f60e97f9debbfaac3c46d
hezil <31658384+hezil@users.noreply.github.com>
2019-06-29 21:40:54 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -36,6 +36,7 @@ variable "ami" {
   description = "ami to use - based on region"
   default = {
     "us-east-1" = "ami-0565af6e282977273"
-    "us-east-2" = "ami-0653e888ec96eab9b"
+#     "us-east-2" = "ami-0653e888ec96eab9b"
+    "us-east-2" = "ami-0450f6efcdce7c116"
   }
 }
