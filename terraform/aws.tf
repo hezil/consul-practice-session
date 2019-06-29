9b269a485c7781cecb93c0c6bab9e5c4b15232bf
hezil <31658384+hezil@users.noreply.github.com>
2019-06-29 22:40:40 +0300

    Update aws.tf

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,12 +1,73 @@
-provider "aws" {
-  region = "${var.region}"
-  version = "~> 1.0"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+# #Get the list of official Canonical Ubunt 14.04 AMIs
+# data "aws_ami" "ubuntu-1404" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm/ubuntu-trusty-14.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["099720109477"] # Canonical
+# }
+
+terraform {
+  backend "s3" {
+    bucket = "us-east-2"
+    key    = "layer1/infrastructure.tfstate"
+    region = "terraform-remote-state-11.03.2019"
+  }
 }
 
-resource "aws_security_group" "opsschool_consul" {
-  name        = "opsschool-consul"
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "consul" {
+  cidr_block           = "${var.vpc_cidr_block}"
+  enable_dns_hostnames = true
+
+  tags {
+    "Name" = "${var.namespace}"
+  }
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "consul" {
+  vpc_id = "${aws_vpc.consul.id}"
+
+  tags {
+    "Name" = "${var.namespace}"
+  }
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.consul.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.consul.id}"
+}
+
+# Grab the list of availability zones
+data "aws_availability_zones" "available" {}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "consul" {
+  count                   = "${length(var.cidr_blocks)}"
+  vpc_id                  = "${aws_vpc.consul.id}"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  cidr_block              = "${var.cidr_blocks[count.index]}"
+  map_public_ip_on_launch = true
+
+  tags {
+    "Name" = "${var.namespace}"
+  }
+}
+
+resource "aws_security_group" "final_project" {
+  name        = "final-project"
   description = "Allow ssh & consul inbound traffic"
 
   ingress {
@@ -26,6 +87,26 @@ resource "aws_security_group" "opsschool_consul" {
   }
 
   ingress {
+    from_port   = 80
+    protocol    = "TCP"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 30036
+    protocol    = "TCP"
+    to_port     = 30036
+    cidr_blocks = ["0.0.0.0/0"]
+      
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+  
+  ingress {
     from_port   = 8500
     to_port     = 8500
     protocol    = "tcp"
@@ -44,26 +125,47 @@ resource "aws_security_group" "opsschool_consul" {
 
 # Create an IAM role for the auto-join
 resource "aws_iam_role" "consul-join" {
-  name               = "opsschool-consul-join"
+  name               = "final-project-consul-join"
   assume_role_policy = "${file("${path.module}/templates/policies/assume-role.json")}"
 }
 
 # Create the policy
 resource "aws_iam_policy" "consul-join" {
-  name        = "opsschool-consul-join"
+  name        = "final-project-consul-join"
   description = "Allows Consul nodes to describe instances for joining."
   policy      = "${file("${path.module}/templates/policies/describe-instances.json")}"
 }
 
 # Attach the policy
 resource "aws_iam_policy_attachment" "consul-join" {
-  name       = "opsschool-consul-join"
+  name       = "final-project-consul-join"
   roles      = ["${aws_iam_role.consul-join.name}"]
   policy_arn = "${aws_iam_policy.consul-join.arn}"
 }
 
 # Create the instance profile
 resource "aws_iam_instance_profile" "consul-join" {
-  name  = "opsschool-consul-join"
+  name  = "final-project-consul-join"
   role = "${aws_iam_role.consul-join.name}"
 }
+
+resource "aws_security_group" "elb_security_group" {
+  name = "ELB-SG"
+  description = "ELB Security Group"
+  vpc_id      = "${aws_vpc.consul.id}"
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow web traffic to load balancer"
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+} 
